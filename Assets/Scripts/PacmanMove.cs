18160c2839ad4a7bed3b1a97e93f4578ebb32c34
Abhinav <abhin2149@gmail.com>
2019-10-15 13:55:09 +0530

    collider update

--- Assets/Scripts/PacmanMove.cs
+++ Assets/Scripts/PacmanMove.cs
@@ -9,7 +9,8 @@ public class PacmanMove : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        dest = transform.position;
+        // dest = transform.position;
+        transform.Translate(dest);
     }
 
     // Update is called once per frame
